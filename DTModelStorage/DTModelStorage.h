7d0cbc0f1fba12f6f439b9431b9c35d53d4d6ee3
Denys Telezhkin <strangervir@gmail.com>
2013-12-22 14:03:17 +0200

    Cleanup, add licenses

--- DTModelStorage/DTModelStorage.h
+++ DTModelStorage/DTModelStorage.h
@@ -1,8 +1,8 @@
 //
-//  DTCOllectionViewSectionModel.m
-//  DTCollectionViewManagerExample
+//  DTModelStorage.h
+//  DTModelStorageTests
 //
-//  Created by Denys Telezhkin on 15.12.13.
+//  Created by Denys Telezhkin on 22.12.13.
 //  Copyright (c) 2013 Denys Telezhkin. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -23,45 +23,5 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import "DTSectionModel.h"
-
-@interface DTSectionModel()
-@property (nonatomic, strong) NSMutableDictionary * supplementaries;
-@end
-
-@implementation DTSectionModel
-
--(NSMutableArray *)objects
-{
-    if (!_objects)
-    {
-        _objects = [NSMutableArray array];
-    }
-    return _objects;
-}
-
--(NSMutableDictionary *)supplementaries
-{
-    if (!_supplementaries)
-    {
-        _supplementaries = [NSMutableDictionary dictionary];
-    }
-    return _supplementaries;
-}
-
--(NSUInteger)numberOfObjects
-{
-    return [self.objects count];
-}
-
--(void)setSupplementaryModel:(id)model forKind:(NSString *)kind
-{
-    self.supplementaries[kind] = model;
-}
-
--(id)supplementaryModelOfKind:(NSString *)kind
-{
-    return self.supplementaries[kind];
-}
-
-@end
+#import "DTMemoryStorage.h"
+#import "DTCoreDataStorage.h"
--- DTModelStorage/DTStorage.h
+++ DTModelStorage/DTStorage.h
@@ -5,9 +5,25 @@
 //  Created by Denys Telezhkin on 15.12.13.
 //  Copyright (c) 2013 Denys Telezhkin. All rights reserved.
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import "DTStorageUpdate.h"
-
 
 /**
  `DTTableViewDataStorageUpdating` protocol is used to transfer data storage updates to `DTTableViewController` object.
@@ -50,29 +66,5 @@
  */
 
 @property (nonatomic, weak) id <DTStorageUpdating> delegate;
-
-@optional
-
-/**
- Method to create searching data storage, based on current data storage. This method will be called automatically by `DTTableViewController` instance.
- 
- @param searchString String, used to search in data storage
- 
- @param searchScope Search scope for current search.
- 
- @return searching data storage.
- */
-
-- (instancetype)searchingStorageForSearchString:(NSString *)searchString
-                                  inSearchScope:(NSInteger)searchScope;
-
-/**
- Getter method for header model for current section.
- 
- @param index Number of section.
- 
- @return Header model for section at index.
- */
-- (id)headerModelForSectionIndex:(NSInteger)index;
 
 @end
