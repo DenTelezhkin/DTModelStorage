9b4c93baeb29fcd0810dfdaf134bd0881268a9f5
Denys Telezhkin <strangervir@gmail.com>
2014-03-03 14:55:58 +0200

    add docs for update without animations method

--- DTModelStorage/Utilities/DTMemoryStorage+UpdateWithoutAnimations.h
+++ DTModelStorage/Utilities/DTMemoryStorage+UpdateWithoutAnimations.h
@@ -10,6 +10,11 @@
 
 @interface DTMemoryStorage (UpdateWithoutAnimations)
 
+/**
+ This method allows multiple simultaneous changes to memory storage without any notifications for delegate. You can think of this as a way of "manual" management for memory storage. Typical usage would be multiple insertions/deletions etc., if you don't need any animations. You can batch any changes in block, and call reloadData on your UI component after this method was call.
+ 
+ @warning You must call reloadData after calling this method, or you will get NSInternalInconsistencyException runtime, thrown by either UITableView or UICollectionView.
+ */
 -(void)updateWithoutAnimations:(void(^)(void))block;
 
 @end
