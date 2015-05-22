0d283195f313e3710e1fee98e007f26e1c3e2c9b
Denys Telezhkin <strangervir@gmail.com>
2015-05-22 15:18:26 +0300

    replace even more new imports with old ones for compatibility reasons

--- DTModelStorage/Core/DTStorageUpdate.h
+++ DTModelStorage/Core/DTStorageUpdate.h
@@ -23,7 +23,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-@import Foundation;
+#import <Foundation/Foundation.h>
 
 #pragma clang assume_nonnull begin
 
--- DTModelStorage/Core/DTStorageUpdating.h
+++ DTModelStorage/Core/DTStorageUpdating.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Denys Telezhkin. All rights reserved.
 //
 
-@import Foundation;
+#import <Foundation/Foundation.h>
 #import "DTStorageUpdate.h"
 
 #pragma clang assume_nonnull begin
