e0295b434ce27c6043d134331afdb8ba0497fd5e
Denys Telezhkin <strangervir@gmail.com>
2015-07-11 16:35:28 +0300

    finish MemoryStorage implementation

--- DTModelStorage/Memory/MemoryStorage.swift
+++ DTModelStorage/Memory/MemoryStorage.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Swift
 
 class MemoryStorage: BaseStorage, StorageProtocol
 {
@@ -38,29 +39,6 @@ class MemoryStorage: BaseStorage, StorageProtocol
             self.delegate?.storageDidPerformUpdate(self.currentUpdate!)
         }
         self.currentUpdate = nil
-    }
-    
-    func sectionAtIndex(sectionIndex : Int) -> SectionModel
-    {
-        self.startUpdate()
-        let section = self.getValidSection(sectionIndex)
-        self.finishUpdate()
-        return section
-    }
-    
-    private func getValidSection(sectionIndex : Int) -> SectionModel
-    {
-        if sectionIndex < self.sections.count
-        {
-            return self.sections[sectionIndex] as! SectionModel
-        }
-        else {
-            for i in self.sections.count...sectionIndex {
-                self.sections.append(SectionModel())
-                self.currentUpdate?.insertedSectionIndexes.addIndex(i)
-            }
-        }
-        return self.sections.last as! SectionModel
     }
     
     func setSectionHeaderModel(model: Any?, forSectionIndex sectionIndex: Int)
@@ -114,6 +92,220 @@ class MemoryStorage: BaseStorage, StorageProtocol
         section.objects.removeAll(keepCapacity: false)
         section.objects.extend(items)
         self.delegate?.storageNeedsReloading()
+    }
+    
+    func addItems(items: [Any], toSection index: Int = 0)
+    {
+        self.startUpdate()
+        let section = self.getValidSection(index)
+        
+        for item in items {
+            let numberOfItems = section.numberOfObjects
+            section.objects.append(item)
+            self.currentUpdate?.insertedRowIndexPaths.append(NSIndexPath(forItem: numberOfItems, inSection: index))
+        }
+        self.finishUpdate()
+    }
+    
+    func addItem(item: Any, toSection index: Int = 0)
+    {
+        self.startUpdate()
+        let section = self.getValidSection(index)
+        let numberOfItems = section.numberOfObjects
+        section.objects.append(item)
+        self.currentUpdate?.insertedRowIndexPaths.append(NSIndexPath(forItem: numberOfItems, inSection: index))
+        self.finishUpdate()
+    }
+    
+    func insertItem(item: Any, toIndexPath indexPath: NSIndexPath)
+    {
+        self.startUpdate()
+        let section = self.getValidSection(indexPath.section)
+        
+        if section.objects.count < indexPath.item {
+            // MARK: - TODO - throw an error in Swift 2.
+            return
+        }
+        section.objects.insert(item, atIndex: indexPath.item)
+        self.currentUpdate?.insertedRowIndexPaths.append(indexPath)
+        self.finishUpdate()
+    }
+    
+    func reloadItem<T:Equatable>(item: T)
+    {
+        self.startUpdate()
+        if let indexPath = self.indexPathForItem(item) {
+            self.currentUpdate?.updatedRowIndexPaths.append(indexPath)
+        }
+        self.finishUpdate()
+    }
+    
+    func replaceItem<T: Equatable, U:Equatable>(itemToReplace: T, replacingItem: U)
+    {
+        self.startUpdate()
+        // MARK: TODO - Use guard and defer in Swift 2
+        let originalIndexPath = self.indexPathForItem(itemToReplace)
+        if originalIndexPath != nil {
+            let section = self.getValidSection(originalIndexPath!.item)
+            section.objects[originalIndexPath!.item] = replacingItem
+        }
+        else {
+            // MARK: TODO - Throw an error in Swift 2
+            self.finishUpdate()
+            return
+        }
+        self.currentUpdate?.updatedRowIndexPaths.append(originalIndexPath!)
+        
+        self.finishUpdate()
+    }
+    
+    func removeItem<T:Equatable>(item: T)
+    {
+        self.startUpdate()
+        
+        // MARK: TODO - Use guard and defer in Swift 2
+        let indexPath = self.indexPathForItem(item)
+        if indexPath != nil {
+            self.getValidSection(indexPath!.section).objects.removeAtIndex(indexPath!.item)
+        }
+        else {
+            // MARK: TODO - Throw an error in Swift 2
+            return
+        }
+        self.currentUpdate?.deletedRowIndexPaths.append(indexPath!)
+        self.finishUpdate()
+    }
+    
+    func removeItems<T:Equatable>(items: [T])
+    {
+        self.startUpdate()
+        let indexPaths = self.indexPathArrayForItems(items)
+        for item in items
+        {
+            if let indexPath = self.indexPathForItem(item) {
+                self.getValidSection(indexPath.section).objects.removeAtIndex(indexPath.item)
+            }
+        }
+        self.currentUpdate?.deletedRowIndexPaths.extend(indexPaths)
+        self.finishUpdate()
+    }
+    
+    func removeItemsAtIndexPaths(indexPaths : [NSIndexPath])
+    {
+        self.startUpdate()
+        
+        let reverseSortedIndexPaths = self.dynamicType.sortedArrayOfIndexPaths(indexPaths, ascending: false)
+        for indexPath in reverseSortedIndexPaths
+        {
+            if let object = self.objectAtIndexPath(indexPath)
+            {
+                self.getValidSection(indexPath.section).objects.removeAtIndex(indexPath.item)
+                self.currentUpdate?.deletedRowIndexPaths.append(indexPath)
+            }
+        }
+        
+        self.finishUpdate()
+    }
+    
+    func deleteSections(sections : NSIndexSet)
+    {
+        self.startUpdate()
+
+        for var i = sections.lastIndex; i != NSNotFound; i = sections.indexLessThanIndex(i) {
+            self.sections.removeAtIndex(i)
+        }
+        self.currentUpdate?.deletedSectionIndexes.addIndexes(sections)
+        
+        self.finishUpdate()
+    }
+}
+
+// MARK: - Searching in storage
+extension MemoryStorage
+{
+    func itemsInSection(section: Int) -> [Any]?
+    {
+        if self.sections.count > section {
+            return self.sections[section].objects
+        }
+        return nil
+    }
+    
+    func itemAtIndexPath(indexPath: NSIndexPath) -> Any?
+    {
+        let sectionObjects : [Any]
+        if indexPath.section < self.sections.count
+        {
+            sectionObjects = self.itemsInSection(indexPath.section)!
+        }
+        else {
+            return nil
+        }
+        if indexPath.row < sectionObjects.count {
+            return sectionObjects[indexPath.row]
+        }
+        return nil
+    }
+    
+    func indexPathForItem<T: Equatable>(searchableItem : T) -> NSIndexPath?
+    {
+        for sectionIndex in 0..<self.sections.count
+        {
+            let rows = self.sections[sectionIndex].objects
+            
+            for rowIndex in 0..<rows.count {
+                if let item = rows[rowIndex] as? T {
+                    if item == searchableItem {
+                        return NSIndexPath(forItem: rowIndex, inSection: sectionIndex)
+                    }
+                }
+            }
+            
+        }
+        return nil
+    }
+    
+    func sectionAtIndex(sectionIndex : Int) -> SectionModel
+    {
+        self.startUpdate()
+        let section = self.getValidSection(sectionIndex)
+        self.finishUpdate()
+        return section
+    }
+    
+    private func getValidSection(sectionIndex : Int) -> SectionModel
+    {
+        if sectionIndex < self.sections.count
+        {
+            return self.sections[sectionIndex] as! SectionModel
+        }
+        else {
+            for i in self.sections.count...sectionIndex {
+                self.sections.append(SectionModel())
+                self.currentUpdate?.insertedSectionIndexes.addIndex(i)
+            }
+        }
+        return self.sections.last as! SectionModel
+    }
+    
+    func indexPathArrayForItems<T:Equatable>(items:[T]) -> [NSIndexPath]
+    {
+        var indexPaths = [NSIndexPath]()
+        
+        for index in 0..<items.count {
+            if let indexPath = self.indexPathForItem(items[index])
+            {
+                indexPaths.append(indexPath)
+            }
+        }
+        return indexPaths
+    }
+    
+    class func sortedArrayOfIndexPaths(indexPaths: [NSIndexPath], ascending: Bool) -> [NSIndexPath]
+    {
+        var unsorted = NSMutableArray(array: indexPaths)
+        let descriptor = NSSortDescriptor(key: "self", ascending: ascending)
+        return unsorted.sortedArrayUsingDescriptors([descriptor]) as! [NSIndexPath]
     }
 }
 
---
+++ DTModelStorage/Utilities/MemoryStorage+UpdateWithoutAnimations.swift
@@ -0,0 +1,20 @@
+//
+//  MemoryStorage+UpdateWithoutAnimations.swift
+//  DTModelStorageTests
+//
+//  Created by Denys Telezhkin on 11.07.15.
+//  Copyright (c) 2015 Denys Telezhkin. All rights reserved.
+//
+
+import Foundation
+
+extension MemoryStorage
+{
+    func updateWithoutAnimations(block: () -> Void)
+    {
+        let delegate = self.delegate
+        self.delegate = nil
+        block()
+        self.delegate = delegate
+    }
+}
