48b57ec076abb0d6b7d4726d4c130b02500cd350
Denys Telezhkin <strangervir@gmail.com>
2017-05-29 18:41:48 +0300

    improved handling of NSFetchedResultsController update change in cases, where object inserts/removals/moves is used simultaneously with object updates(#17). Closes #17.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,8 @@ All notable changes to this project will be documented in this file.
 
 # Next
 
+* Improved handling of `NSFetchedResultsControllerDelegate` `NSFetchedResultsChangeType.update` change type in cases, where object inserts/removal/moves is used simultaneously with object updates(#17).
+
 ## [5.0.0](https://github.com/DenHeadless/DTModelStorage/releases/tag/5.0.0)
 
 * Reworked `EventReaction` class to use `ViewModelMapping` to properly identify not only model and `ViewType`, but also `viewClass`. This allows event reactions to run for cases where two view subclasses, conforming to `ModelTransfer`, use the same model, and have similar events.
--- Source/Core/CoreDataStorage.swift
+++ Source/Core/CoreDataStorage.swift
@@ -136,8 +136,14 @@ open class CoreDataStorage<T: NSFetchRequestResult> : BaseStorage, Storage, Supp
             }
         case .update:
             if let indexPath = indexPath {
-                currentUpdate?.objectChanges.append((.update, [indexPath]))
-                currentUpdate?.updatedObjects[indexPath] = anObject
+                if let newIndexPath = newIndexPath, indexPath != newIndexPath {
+                    currentUpdate?.objectChanges.append((.delete,[indexPath]))
+                    currentUpdate?.objectChanges.append((.insert,[newIndexPath]))
+                }
+                else {
+                    currentUpdate?.objectChanges.append((.update,[indexPath]))
+                    currentUpdate?.updatedObjects[indexPath] = anObject
+                }
             }
         }
     }
